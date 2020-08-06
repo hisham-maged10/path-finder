0e6114db3a74ab28e940d148e8e559b47bc56228
hisham-maged10 <Hisham.maged10@gmail.com>
2020-08-06 21:28:12 +0200

    fixed RT path

--- script/common.js
+++ script/common.js
@@ -12,7 +12,7 @@ function getPath(parentMap,curr){
 function drawPath(counter,path){
   if(counter !== path.length){
     const curr = path[counter];
-    curr.divReference.classList.add("node-path-rt");
+    curr.divReference.classList.add("node-path");
     setTimeout(drawPath,45,++counter,path);
   }else{
     document.querySelector("#clear").disabled = false;
@@ -30,7 +30,7 @@ function drawPathRT(path){
   let counter = 0;
   while(counter !== path.length){
     const curr = path[counter];
-    curr.divReference.classList.add("node-path");
+    curr.divReference.classList.add("node-path-rt");
     ++counter;
   }
 }
